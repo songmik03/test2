997f4b9a36745a608c1680bb6dcd635f9c1f9cba
Song <songmik03@gmail.com>
2016-07-20 09:05:28 +0900

    ....

---
+++ day16_h/ex2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int array[] = {1,2,3,4,5,6,7,8};
+int rangeSize = sizeof(array)/sizeof(int);
+
+void change(int rangeSize)
+{
+	int temp[10];
+	memcpy(temp,array,sizeof(array));
+	
+	for(int i=0;i<rangeSize;i++) {
+		array[(rangeSize-1)-i] = temp[i];
+	}
+
+}
+int main()
+{
+	change(rangeSize);	
+	
+	for(int i=0;i<rangeSize;i++) {
+		printf("%d,", array[i]);
+	}
+	return 0;
+}
